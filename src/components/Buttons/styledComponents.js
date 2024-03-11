365be4269b46611a9e99fc3ea587755790048a72
sneharika <snehagunde1@gmail.com>
2024-03-11 12:20:26 +0000

    Rock-Paper-Scissors

---
+++ src/components/Buttons/styledComponents.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const ButtonLiContainer = styled.li`
+list-style: none;
+display: flex;
+flex-direction: row;
+justify-content: center;
+width: 50%;
+`
+export const Buttonimage = styled.button`
+background-color: transparent;
+border: 0;
+cursor: pointer;
+`
+
+export const ImageItem = styled.img`
+width: 150px;
+height: 150px;
+`
